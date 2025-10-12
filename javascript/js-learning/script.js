f248dd04d91c9742d531bbc2b1e99df5494164b9
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-10-12 08:35:17 +0500

    variables, values, data types and operators in js

--- javascript/js-learning/script.js
+++ javascript/js-learning/script.js
@@ -3,3 +3,38 @@ console.log("Hello, Bilal");
 console.log("Hello, CloudBreexe Family");
 console.log("Hello, Everyone");
 console.log(5);
+
+console.log('Information')
+
+let name = 'Muhammad Bilal';
+var age = 25;
+let Major = 'Software Engineering';
+let uni = 'Islamia College University Peshawar';
+
+console.log(name)
+console.log(age)
+console.log(Major)
+console.log(uni)
+
+var age = 26;
+
+console.log(name)
+console.log(age)
+console.log(Major)
+console.log(uni)
+
+// basic calculator using arithmetic operators
+let firstNumber = prompt('Enter your first Number:')
+let secondNumber = prompt("Enter your second Number:")
+
+let sum = firstNumber + secondNumber;
+let subtract = firstNumber - secondNumber;
+let multiply = firstNumber * secondNumber;
+let divide = firstNumber / secondNumber;
+
+console.log('Sum of your Two Numbers:', sum);
+console.log('Stbtraction of your Two Numbers:', subtract);
+console.log('Multiplication of your Two Numbers:', multiply);
+console.log('Division of your Two Numbers:', divide);
+
+
