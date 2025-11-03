a11b1f063724c2e436592cd4d599c0a3e4a17de3
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-03 08:41:20 +0500

    todo list done

--- html-css-js-projects/todo-list/script.js
+++ html-css-js-projects/todo-list/script.js
@@ -2,4 +2,44 @@ let inputField = document.querySelector('input')
 let submitBtn = document.querySelector('.submit')
 let clearAllBtn = document.querySelector('.clearall')
 let listItems = document.querySelector('ul')
-console.log(listItems)
+
+
+const handleSubmit = () => {
+
+    let task = inputField.value;
+
+    let newItem = document.createElement('li')
+    newItem.innerHTML = ` 
+                    <p>${task}</p>
+                    <div>
+                        <button class='check'><i class="fa-solid fa-check" style="color: green;"></i></button>
+                        <button class='delete'><i class="fa-solid fa-trash" style="color: red;"></i></button>
+                    </div> `
+
+     if (inputField.value.trim() === "") {
+      alert("Please enter something before submitting!");
+      return; 
+    }
+
+    inputField.value = '';
+
+    let checkTask = newItem.querySelector('.check')
+    checkTask.addEventListener('click', ()=>{newItem.querySelector('p').style.textDecoration = 'line-through'}) 
+    
+    
+    let checkDelete = newItem.querySelector('.delete')
+    checkDelete.addEventListener('click', ()=> listItems.removeChild(newItem))         
+
+    
+
+    listItems.appendChild(newItem)
+    
+}
+const handleClear = () => {
+    listItems.innerHTML = '';
+}
+
+
+
+submitBtn.addEventListener('click', handleSubmit);
+clearAllBtn.addEventListener('click', handleClear);
