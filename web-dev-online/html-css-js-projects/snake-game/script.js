822425980b1468fc4b188cd9f66be949cb36001e
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-14 08:35:15 +0500

    working on js project

--- web-dev-online/html-css-js-projects/snake-game/script.js
+++ web-dev-online/html-css-js-projects/snake-game/script.js
@@ -5,8 +5,30 @@ const blockHeight = 30
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 
-for(let i = 0; i<rows*cols; i++){
+const blocks = [];
+const snake = [{
+    x: 1,
+    y: 3,
+},{
+    x: 1,
+    y: 4,
+},{
+    x: 1,
+    y: 5,
+}];
+
+for(let row = 0; row < rows ; row++){
+    for(let col = 0; col < cols; col++){
     const block = document.createElement('div')
     block.classList.add('block')
     board.appendChild(block)
+    blocks[`${row}${col}`] = block 
+    }
+}
+
+const render = () => {
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+    
 }
