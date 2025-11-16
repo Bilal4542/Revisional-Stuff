8edd34bdf05ad864cfe5f32b860d059ab116ec1b
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-16 07:51:03 +0500

    states and conditions

--- web-dev-online/react/exercises/src/components/Counter.jsx
+++ web-dev-online/react/exercises/src/components/Counter.jsx
@@ -3,6 +3,18 @@ import React, { useState } from 'react'
 const Counter = () => {
     const [count, setCount] = useState(0)
 
+    const [name, setName] = useState('Muhammad')
+
+    const [age, setAge] = useState(0)
+
+    const [user, setUser] = useState({
+        name: 'Muhammad Bilal',
+        age: 12,
+        address: 'Peshawar',
+        role: 'Software Engineering'
+    })
+
+
     const increment = () =>{
         setCount(count + 1)
     }
@@ -12,12 +24,38 @@ const Counter = () => {
         
     }
 
+    const nameHandler = () => {
+        setName('Muhammad Bilal')
+    }
+
+    const ageHandler = () => {
+        setAge(Math.floor(Math.random()*50))
+    }
+
   return (
+    <>
     <div>
       <button onClick={increment}>Plus</button>
       <p>{count}</p>
       <button onClick={decrement}>Minus</button>
     </div>
+    <div className="">
+        <p>{name}</p>
+        <button onClick={nameHandler}>Get Full Name</button>
+    </div>
+    <div className="">
+        <p>{age}</p>
+        <button onClick={ageHandler}>Get Age</button>
+    </div>
+    <div className="">
+        <h1>{user.name}</h1>
+        <h2>{user.age}</h2>
+        <h3>{user.address}</h3>
+        <h4>{user.role}</h4>
+        <h2>{user.age > 18 ? <p>Eligible for Vote</p> : <p>Not Eligible for Vote</p>}</h2>
+    </div>
+    </>
+    
   )
 }
 
