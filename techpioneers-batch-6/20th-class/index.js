fc1e4996b5de49d1f0fb0a7965679bf482371b4e
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-18 08:31:25 +0500

    task of class 20 done

--- techpioneers-batch-6/20th-class/index.js
+++ techpioneers-batch-6/20th-class/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world')
+// console.log('Hello world')
+
+const myName = 'Muhammad Bilal';
+const myAge = 24;
+const myCountry = 'Pakistan'
+console.log(myName)
+console.log(myAge)
+console.log(myCountry)
+
+
+let city = 'Lahore'
+let anotherCity = city
+console.log(anotherCity)
+
+let a = 5
+let b = 10
+let c = a
+a = b
+b = c
+console.log(a,b)
