0c87d3693450091e1da9077cb4ebf4a7791cce74
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-19 08:40:55 +0500

    task done

--- techpioneers-batch-6/21th-class/index.js
+++ techpioneers-batch-6/21th-class/index.js
@@ -2,16 +2,38 @@
 // operators arithematic (+, -, * , /) , logical (&&, ||, !), comaprison (=, ==, ===, <, >, <=, >= !=) operators
 
 
-const myAge = 24;
-if(myAge > 18){
-    console.log('I am adult')
+// const myAge = 24;
+// if(myAge > 18){
+//     console.log('I am adult')
+// }else{
+//     console.log('I am not adult')
+// }
+
+// const num = 23
+// if(num >= 0){
+//     console.log('positve number')
+// }else{
+//     console.log('negative number')
+// }
+
+
+let num = 17
+if(num % 2 === 0){
+    console.log('The Number is Even')
 }else{
-    console.log('I am not adult')
+    console.log('The Number is Odd')
 }
 
-const num = 23
-if(num >= 0){
-    console.log('positve number')
+let personAge = 23
+if(personAge >= 18){
+    console.log('Yes, Eligible for Vote')
 }else{
-    console.log('negative number')
+    console.log('No, Not Eligible for Vote')
+}
+
+let password = '12345'
+if(password === '12345'){
+    console.log('Login Successful')
+}else{
+    console.log('inValid Password')
 }
