da5ec635316e3db77e709d6974166fee59caf09a
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-19 08:01:39 +0500

    class 21 done

---
+++ techpioneers-batch-6/21th-class/index.js
@@ -0,0 +1,17 @@
+// if else today topics
+// operators arithematic (+, -, * , /) , logical (&&, ||, !), comaprison (=, ==, ===, <, >, <=, >= !=) operators
+
+
+const myAge = 24;
+if(myAge > 18){
+    console.log('I am adult')
+}else{
+    console.log('I am not adult')
+}
+
+const num = 23
+if(num >= 0){
+    console.log('positve number')
+}else{
+    console.log('negative number')
+}
