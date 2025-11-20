1a0a94a99a81d589a6fb1015f4ff3b1bd5345af3
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-11-20 06:57:20 +0500

    class 22 done

---
+++ techpioneers-batch-6/22th-class/index.js
@@ -0,0 +1,7 @@
+/*
+    arrays => array should be in square brackets [], a set of data, index of array starts from 0 index, 
+    methoods of array => push for adding new item in the last of array, pop deleted the value at the end of the array , unshift added the value at the start of the array, shift deleted the value at the start of the array, splice added and deleted the value from where u want, 
+*/ 
+
+console.log('hello world')
+
