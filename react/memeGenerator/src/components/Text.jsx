cc655601780fe7a57abdaf8c283a02e9008fda4a
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-12-21 18:55:02 +0500

    working on meme generator

--- react/memeGenerator/src/components/Text.jsx
+++ react/memeGenerator/src/components/Text.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { motion } from "framer-motion";;
 
 const Text = () => {
   return (
-    <div>
-        <h2 className='text-2xl'>
-      I am Text
-        </h2>
-    </div>
+      // <Draggable bounds='parent'>
+      //   <h2 className='text-2xl'>I am Text</h2>
+      // </Draggable>
+      <>
+      <motion.div
+        drag                  // Makes it draggable
+        dragMomentum={true}   // Adds inertia when you release
+        className="cursor-move select-none">
+        <h2 className="text-2xl font-bold">I am draggable</h2>
+      </motion.div>
+      </>
   )
 }
 
